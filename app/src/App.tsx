65a82616c71b543704e0b44bd33b2b7ea67221f1
Super_Crafting <pedroerafa2005@hotmail.com>
2025-12-18 10:18:45 +0100

    refactor: clean

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,13 +1,16 @@
+import { CookiesProvider } from "react-cookie";
 import { RestProvider } from "./hooks/rest";
 
 export default function App() {
     return (
-		<div>
-			<RestProvider
-				url="http://localhost:8000"
-			>
-				
-			</RestProvider>
+		<div style={{ height: '100vh', display: 'flex', flexDirection: 'column' }}>
+			<CookiesProvider>
+				<RestProvider
+					url="http://localhost:8000"
+				>
+					
+				</RestProvider>
+			</CookiesProvider>
 		</div>
 	)
 }
--- app/vite.config.ts
+++ app/vite.config.ts
@@ -4,4 +4,17 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  optimizeDeps: {
+    // Cornerstone codecs (UMD/CJS) doivent être pré-bundlés par Vite,
+    // sinon les imports `default` peuvent casser en dev.
+    include: [
+      "@cornerstonejs/dicom-image-loader",
+      "@cornerstonejs/codec-libjpeg-turbo-8bit/decodewasmjs",
+      "@cornerstonejs/codec-libjpeg-turbo-8bit/wasmjs",
+      "@cornerstonejs/codec-charls",
+      "@cornerstonejs/codec-openjpeg",
+      "@cornerstonejs/codec-openjph",
+    ],
+    needsInterop: ["@cornerstonejs/codec-libjpeg-turbo-8bit"],
+  },
 })
